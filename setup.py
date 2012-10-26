82ca8b9f127990eee143437245b04f70ac282eb4
Bryan Helmig <bryan@zapier.com>
2012-10-25 18:50:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="pyrise",
-      version='0.4.3',
+      version='0.4.4',
       description="Python wrapper for 37Signals Highrise",
       long_description="A work in progress, but one that will be awesome when finished. Pyrise gives you class objects that work a lot like Django models, making the whole experience of integrating with Highrise just a little more awesome and Pythonic.",
       license="MIT License",
